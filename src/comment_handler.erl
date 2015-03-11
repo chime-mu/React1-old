4d27892876aac284c13215f161d20d0635756c7c
Michael Arnoldus <chime@mu.dk>
2015-03-11 15:24:35 +0100

    Changed handler to act as cowboy rest interface and added POST functionality

--- src/comment_handler.erl
+++ src/comment_handler.erl
@@ -1,24 +1,39 @@
 -module(comment_handler).
--behaviour(cowboy_http_handler).
 
 -export([init/3]).
--export([handle/2]).
 -export([terminate/3]).
 -export([addcomment/2]).
+-export([allowed_methods/2, 
+	     content_types_provided/2, 
+	     content_types_accepted/2,
+	     to_json/2,
+	     from_www_form/2]).
 
--record(state, {}).
 -record(comment, {author, text}).
 
-init(_, Req, _Opts) ->
-	{ok, Req, #state{}}.
+init({tcp, http}, _Req, _Opts) ->
+	{upgrade, protocol, cowboy_rest}.
 
-handle(Req, State=#state{}) ->
+allowed_methods(Req, State) ->
+	{[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>, <<"POST">>], Req, State}.
+
+content_types_provided(Req, State) ->
+	{[{{<<"application">>, <<"json">>, '*'}, to_json}], Req, State}.
+
+content_types_accepted(Req, State) ->
+	Content_types = [{{<<"application">>, <<"x-www-form-urlencoded">>, '*'}, from_www_form}],
+	{Content_types, Req, State}.
+
+to_json(Req, State) -> % json handler
     Comments = ets:lookup_element(store, comments, 2),
-	{ok, Req2} = cowboy_req:reply(200, 
-		[{<<"content-type">>, <<"application/json">>}],
-		n2o_json:encode(lists:map(fun comment2json/1,Comments)),
-		Req),
-	{ok, Req2, State}.
+    Reply = n2o_json:encode(lists:map(fun comment2json/1,Comments)),
+	{Reply, Req, State}.
+
+from_www_form(Req, State) ->
+	{ok, QS, Req2} = cowboy_req:body_qs(Req),
+	[{<<"author">>,Author},{<<"text">>, Text}] = QS,
+	addcomment(Author, Text),
+	{true, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
 	ok.
@@ -28,7 +43,7 @@ comment2json(#comment{author=Author, text=Text}) ->
 
 addcomment(Author, Text) ->
 	Comments = ets:lookup_element(store, comments, 2),
-	NewComments = [#comment{author=Author, text=Text} | Comments],
+	NewComments = Comments ++ [#comment{author=Author, text=Text}],
 	ets:insert(store,{comments, NewComments}).
 
 
